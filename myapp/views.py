32fe14fa4a0ac16ce683dbb4e1951bb0d973d82c
Franco Lepore Cariolo <francoleporecariolo@gmail.com>
2025-04-10 22:58:58 -0300

    hasta ahora meti todo excepto las url

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Estudiante, Profesor, Curso, Entregable
 
-# Create your views here.
+def lista_estudiantes(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'myapp/estudiantes_list.html', {'estudiantes': estudiantes})
+
+def detalle_estudiante(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    return render(request, 'myapp/estudiante_detail.html', {'estudiante': estudiante})
+
+#Estas vistas obtienen datos de los modelos y los pasan a las plantillas para su presentación.
